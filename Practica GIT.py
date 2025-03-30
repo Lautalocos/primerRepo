2c98a7373e99dfe475b3b05d34229c12507805c9
Lautaro <lautilaner@gmail.com>
2025-03-30 19:45:59 -0300

    se agregaron comentarios a ejercicio 1

--- Practica GIT.py
+++ Practica GIT.py
@@ -1,4 +1,8 @@
+#Se define variable nombre
 nombre = "lautaro"
+#se define variable apellido
 apellido = "laner"
+#se define variable materia
 materia = "programacion 1"
+#se imprime mensaje por consola
 print (f"hola soy {nombre} {apellido}, alumno de {materia}")
